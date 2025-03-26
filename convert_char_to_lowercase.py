999b485e2eb6b64fc1e40b670590b8118ab6ba9e
Gerald <geraldtrogado@gmail.com>
2025-03-27 01:22:21 +0800

    Added pseudo code to 2 files that convert char to either uppercase or lowercase

---
+++ convert_char_to_lowercase.py
@@ -0,0 +1,4 @@
+#Pseudo Code:
+#1. Take the input from the user
+#2. Convert the input to lowercase
+#3. Print the output
---
+++ convert_char_to_uppercase.py
@@ -0,0 +1,4 @@
+#Pseudo Code:
+#1. Take the input from the user
+#2. Convert the input to uppercase
+#3. Print the output
