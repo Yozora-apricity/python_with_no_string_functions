90b73645aee93957c015f8f33fb7016b06f066b9
Gerald <geraldtrogado@gmail.com>
2025-03-27 02:30:55 +0800

    Added my_capitalize function to convert the first character of a string to uppercase

--- make_first_char_uppercase.py
+++ make_first_char_uppercase.py
@@ -2,3 +2,13 @@
 # 1. Ask user to enter a string
 # 2. Convert the first character of the string to uppercase
 # 3. Print the string with first character in uppercase
+
+def my_capitalize(s):
+    if not s:
+        return s
+    first_char = s[0]
+    rest_of_string = s[1:]
+    return first_char.upper() + rest_of_string.lower()
+
+s = input("Enter a detailed string: ")
+print(f'Ouput: {my_capitalize(s)}')
