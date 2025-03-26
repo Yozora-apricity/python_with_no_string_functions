74cbbbcbad5671c451ed144ceb56dd2a0b730e51
Gerald <geraldtrogado@gmail.com>
2025-03-27 01:33:25 +0800

    Added def function and input function to lowercase file then copy the same function and changed capital "A" to "a' to convert it to capital or uppercase

--- convert_char_to_lowercase.py
+++ convert_char_to_lowercase.py
@@ -2,3 +2,15 @@
 #1. Take the input from the user
 #2. Convert the input to lowercase
 #3. Print the output
+
+def to_lowercase(s):
+    result = ""
+    for char in s:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+    return result
+
+input_string = input("Enter a string: ")
+print(to_lowercase(input_string))
--- convert_char_to_uppercase.py
+++ convert_char_to_uppercase.py
@@ -2,3 +2,15 @@
 #1. Take the input from the user
 #2. Convert the input to uppercase
 #3. Print the output
+
+def to_uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+    return result
+
+input_string = input("Enter a string: ")
+print(to_uppercase(input_string))
