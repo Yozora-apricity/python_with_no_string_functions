02f73c46a0dd6345893a0229c69748e3fe7df3eb
Gerald <geraldtrogado@gmail.com>
2025-03-27 00:12:03 +0800

    Added function comments that explains how each function works.

--- strip_leading_spaces.py
+++ strip_leading_spaces.py
@@ -3,11 +3,12 @@
 # Print the substring starting from that index.
 
 def remove_leading_spaces(s):
-    space_index = 0
+    space_index = 0 #Tracks the index of the first non-space character
     while space_index < len(s) and s[space_index] == ' ':
         space_index += 1
     return s[space_index:]
 
+#Ask the user to enter a string and print the Original and Without Front Space string.
 string = input("Enter a string: ")
 print(f"Original: '{string}'")
 print(f"Without Front Space: '{remove_leading_spaces(string)}'")
