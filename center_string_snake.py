3234bea0814a1cecfbe664937ad5d5f1384a1fa2
Gerald <geraldtrogado@gmail.com>
2025-03-27 20:03:50 +0800

    Added user input functionality to center_string_snake.py for dynamic string centering based on specified width.

--- center_string_snake.py
+++ center_string_snake.py
@@ -6,3 +6,19 @@
 #    b. Split padding into left (half of total) and right (remaining)
 #    c. Create new string: left spaces + original string + right spaces
 # 4. Print the resulting centered string
+
+# Read the input string and desired width from the user
+sentence = input("Enter the string: ")
+width = int(input("Enter the width: "))
+
+# Check if the width is less than or equal to the string length
+if len(sentence) >= width:
+    print(sentence)
+else:
+    # Calculate total padding and split into left and right
+    total_padding = width - len(sentence)
+    left = total_padding // 2
+    right = total_padding - left
+    # Construct the centered string
+    centered = ' ' * left + sentence + ' ' * right
+    print(centered)
